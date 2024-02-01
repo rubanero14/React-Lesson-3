0b46ef5b2cc6a1f11a7f874d34461d276d3bed5d
Rajaruban Rajindram <85555894+rubanero14@users.noreply.github.com>
2024-02-01 15:00:03 +0800

    creating core concept list item component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,33 @@
-import ReactImagePath from "./assets/react-core-concepts.png";
+import { CORE_CONCEPTS } from "./data";
+import ReactImg from "./assets/react-core-concepts.png";
 
 const dynamicDescWords = ["Fundamental", "Crucial", "Core"];
-
 const getRandomIdx = (arrLength) => {
   return Math.floor(Math.random() * (arrLength + 1));
 };
+
+export function CoreConcept({
+  src = "",
+  title = "",
+  description = "",
+  key = "",
+  id = "",
+}) {
+  return (
+    <li key={key} id={id}>
+      <img src={src} alt={title} />
+      <h1>{title}</h1>
+      <p>{description}</p>
+    </li>
+  );
+}
 
 function Header() {
   const randomDesc =
     dynamicDescWords[getRandomIdx(dynamicDescWords.length - 1)];
   return (
     <header>
-      <img src={ReactImagePath} alt="Stylized atom" />
+      <img src={ReactImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
         {randomDesc} React concepts you will need for almost any app you are
@@ -26,7 +42,20 @@ function App() {
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            {CORE_CONCEPTS.map((data, index) => (
+              <CoreConcept
+                key={index}
+                id={index}
+                src={data.image}
+                title={data.title}
+                description={data.description}
+              />
+            ))}
+          </ul>
+        </section>
       </main>
     </div>
   );
